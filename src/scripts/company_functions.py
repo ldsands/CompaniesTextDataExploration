beadc153e33178f823fd44c758cb143aa88e0f58
ldsands <7889104+ldsands@users.noreply.github.com>
2025-02-24 11:43:59 -0600

    added month and year versions for dictionary graphs added colors for each company added graph to proportional companies separate from each other for one dictionary

---
+++ src/scripts/company_functions.py
@@ -0,0 +1,9 @@
+def company_colors_dict() -> dict:
+    return {
+        # TODO: get the colors for each company ideally from their brand identity
+        "Apple": "#009B00",
+        "Google": "#0000FF",
+        "Microsoft": "#FFFF00",
+        "Targe": "#00FFFF",
+        "Walmart": "#FF00FF",
+    }
