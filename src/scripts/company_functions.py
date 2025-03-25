55d524ded3c0669d6be262bf80e53f12b92a3c5a
ldsands <7889104+ldsands@users.noreply.github.com>
2025-03-25 18:11:49 -0500

    updated to include amazon data

--- src/scripts/company_functions.py
+++ src/scripts/company_functions.py
@@ -1,6 +1,7 @@
 def company_colors_dict() -> dict:
     return {
         # TODO: get the colors for each company ideally from their brand identity
+        "Amazon": "#FB9700",
         "Apple": "#009B00",
         "Google": "#0000FF",
         "Microsoft": "#FFFF00",
