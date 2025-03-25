8a68774f23c3010b241e3879f070d9c66f566386
ldsands <7889104+ldsands@users.noreply.github.com>
2025-03-25 18:20:07 -0500

    added colors for each company

--- src/scripts/company_functions.py
+++ src/scripts/company_functions.py
@@ -1,10 +1,10 @@
 def company_colors_dict() -> dict:
     return {
         # TODO: get the colors for each company ideally from their brand identity
-        "Amazon": "#FB9700",
-        "Apple": "#009B00",
-        "Google": "#0000FF",
-        "Microsoft": "#FFFF00",
-        "Targe": "#00FFFF",
-        "Walmart": "#FF00FF",
+        "Amazon": "#FF9900",
+        "Apple": "#A3AAAE",
+        "Google": "#2DA94F",
+        "Microsoft": "#00A4EF",
+        "Target": "#CC0000",
+        "Walmart": "#FCB61A",
     }
